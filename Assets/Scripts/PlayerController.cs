431281fdb29f136dafb5bfc49a261460d87a995a
AMeghnad <anshumanmeghnad@gmail.com>
2018-11-05 15:47:03 +1100

    movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    [SerializeField]
+    float inputH, inputV;
+    float speed = 15f;
+    float gravity = -1f;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(inputH, gravity, inputV) * Time.deltaTime * speed;
+        controller.Move(direction);
+    }
 }
