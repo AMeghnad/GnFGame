c99d5dc6273eeb20472ea4343bcc0c799d23547f
AMeghnad <anshumanmeghnad@gmail.com>
2018-11-13 15:35:51 +1100

    Small script changes

---
+++ Assets/Scripts/Camera/CameraSpin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpin : MonoBehaviour
+{
+
+    public Transform target;
+    public Camera cam;
+    public float str;
+    void Start ()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        cam = Camera.main;
+	}
+	void Update ()
+    {
+       
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, str);
+    
+    }
+}
